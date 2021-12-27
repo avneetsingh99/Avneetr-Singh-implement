9e3571ce126460da6ef0514142efc1e337ea36cd
avneetsingh99 <96041683+avneetsingh99@users.noreply.github.com>
2021-12-27 12:56:42 +0530

    Add files via upload

---
+++ Avneet Singh Implement.py
@@ -0,0 +1,26 @@
+for i in range(7,2,-1):
+    for j in range(1,i-1):
+        print(j,end=" ")
+    print("\r")
+#b
+for x in range(5,0,-1):
+    for y in range(x,0,-1):
+       print(y,end =" ")
+    print("\r")
+#c
+for s in range(5):
+    for r in range(s+1):
+        print('*',end=" ")
+    print(" ")
+for s in range(5,1,-1):
+    for r in range(s-1):
+        print('*',end=" ")
+    print(" ")
+
+    
+
+
+
+
+       
+
